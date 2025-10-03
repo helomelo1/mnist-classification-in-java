57c0100c4b2865b9d244d161ae1334d13e796fb9
helomelo1 <b23ee1069@iitj.ac.in>
2025-10-03 23:38:39 +0530

    minor changes

---
+++ main.java
@@ -0,0 +1,8 @@
+class main{
+    public static void main(String[] arg) {
+        // System.out.println("Hello, World!");
+
+        int age = 5;
+        System.out.println("My age is " + age + ".");
+    }
+}
